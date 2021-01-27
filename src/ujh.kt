7acad050ee8eb986fb7777689eb5125378384532
user1 <user1>
2021-01-27 18:54:00 +0700

    Initial commit

---
+++ src/ujh.kt
@@ -0,0 +1,11 @@
+fun main (){
+    var a = 100
+        var s = 786
+            var d = a+s
+    var f =6
+        var g =7
+    if (d <800)
+        println(f)
+    else (d >700)
+        println(g)
+}
